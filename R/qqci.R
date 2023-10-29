190601c34045b8ccac06b986adaa6d761ab0e8f6
Benjamin M. Taylor <benjamin.taylor.software@gmail.com>
2023-10-29 10:40:02 +0000

    version 1.5-7

--- R/qqci.R
+++ R/qqci.R
@@ -12,10 +12,13 @@
 ##' @param zero logical, whether to zero the plot across the x-axis. Default is FALSE
 ##' @param standardise logical, whether to standardise so that the variance around each quantile is made constant (this can help in situations where the confidence bands appear very tight in places)
 ##' @param qts vector of probabilities giving which sample-based empirical quantiles to add to the plot. Default is c(0.025,0.975)
-##' @param lwd positive numeric, the width of line to plot
+##' @param llwd positive numeric, the width of line to plot, default is 2
+##' @param lcol colour of line to plot, default is red
 ##' @param xlab character, the label for the x-axis
 ##' @param ylab character, the label for the y-axis
 ##' @param alpha controls transparency of samples (coloured blue)
+##' @param cicol colour of confidence band lines, default is black
+##' @param cilwd width of confidence band lines, default is 1
 ##' @param ... additional arguments to pass to matplot
 ##' @return Produces a QQ-plot with simulation-based confidence bands
 ##' @examples qqci(rnorm(1000))
@@ -23,7 +26,7 @@
 ##' @examples qqci(rnorm(1000),zero=TRUE,standardise=TRUE)
 ##' @export
 
-qqci = function(x,rfun=NULL,y=NULL,ns=100,zero=FALSE,standardise=FALSE,qts=c(0.025,0.975),lwd=2,xlab="Theoretical",ylab="Sample",alpha=0.1,...){
+qqci = function(x,rfun=NULL,y=NULL,ns=100,zero=FALSE,standardise=FALSE,qts=c(0.025,0.975),llwd=2,lcol="red",xlab="Theoretical",ylab="Sample",alpha=0.02,cicol="black",cilwd=1,...){
 
     if(!is.null(rfun) & !is.null(y)){
         stop("Must specify exactly one of 'rfun' or 'y'.")
@@ -38,7 +41,9 @@ qqci = function(x,rfun=NULL,y=NULL,ns=100,zero=FALSE,standardise=FALSE,qts=c(0.0
         }
     }
     else{
-        rfun = rnorm
+        if(is.null(rfun)){
+            rfun = rnorm
+        }
     }
     
     x = sort(x)
@@ -59,8 +64,8 @@ qqci = function(x,rfun=NULL,y=NULL,ns=100,zero=FALSE,standardise=FALSE,qts=c(0.0
     }
     qt = apply(smp,1,quantile,probs=qts,type=1)
 
-    matplot(mns,smp,type="l",col=rgb(0,0,1,alpha=alpha),lwd=lwd,lty="solid",xlab=xlab,ylab=ylab,...)
-    lines(mns,x,lwd=lwd,col="red")
-    matplot(mns,t(qt),type="l",lty="solid",col="green",lwd=lwd,add=TRUE)
+    matplot(mns,smp,type="l",col=rgb(0,0,1,alpha=alpha),lwd=llwd,lty="solid",xlab=xlab,ylab=ylab,...)
+    lines(mns,x,lwd=llwd,col=lcol)
+    matplot(mns,t(qt),type="l",lty="solid",col=cicol,lwd=cilwd,add=TRUE)
 
 }
